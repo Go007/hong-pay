2b939111635a79139b8cffcfca776db1f59bcb32
王宏 <wanghong3@dafycredit.com>
2018-12-20 10:29:49 +0800

    init

---
+++ hong-pay-service/src/main/java/com/hong/Application_Pay.java
@@ -0,0 +1,13 @@
+package com.hong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application_Pay
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(Application_Pay.class, args);
+    }
+}
---
+++ hong-pay-service/src/test/java/com/hong/AppTest.java
@@ -0,0 +1,20 @@
+package com.hong;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple Application_Pay.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
